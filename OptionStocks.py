8fdfc494d337242738546d124c27061fdeb66189
Dillon Gorlesky <49655577+dmgorlesky1@users.noreply.github.com>
2021-10-09 22:37:36 -0400

    Update OptionStocks.py

--- OptionStocks.py
+++ OptionStocks.py
@@ -1,100 +1,87 @@
 from yahoo_fin.options import *
 from yahoo_fin import options
 from yahoo_fin.stock_info import *
+import requests
 from pandas import *
 import os
 import sys
 import csv
 import math
+from difflib import Differ
+from datetime import date
+import shutil
 
-#This is the file I will keep track of the last index of
-#CSV file ran so I don't have to restart if an error occurs.
-file1 = open("myFile.txt", "r")
+#STEPS
+#1. Read in file used to keep track of previous logs
+#2. Read in FinalTickers.csv
+#3. Compare each line in the two files. PreviousLogs will have a field either 'Y' or 'N' if it had options last
+#       time the program was ran.
+#4a. Write to a temp file an updated options log
+#4b. Print to screen if a ticker is found to have recently had options added.
+#5. Merge the temp file into the previous logs file
+#6. Delete temp file
 
-pos = file1.readline()
-try:
-    #If it didn't get to the first 20 spots
-    if(int(pos) < 20):
-        pos = "0"
-except:
-    pos = "0"
-    
 
-file2 = open("myFile.txt", "w")
-file2.write(str(pos))
-file2.close()
+#The file holding the previous logs of if a ticker had options or not last time the program was ran.
+prevLog = open("previousLog.csv", "r+")
 
-#This will read in the csv file --> Works as relative path bc it's in same directory
-#df = pd.read_excel (r'StockTickerTester.xlsx')
-#df = pd.read_excel (r'StocksWithOptions.xlsx')
-df = pd.read_excel (r'StockTickers.xlsx')
-#df = pd.read_excel (r'AssortedStocks.xlsx')
+#This will allow for me to compare as the tickers are being checked for options
+content = prevLog.readlines()
 
-#print(df)
+#The file all the filtered stocks get written to from NYSE and NASDAQ listings
+finalFile = open("finalTickers.csv", "r")
 
-#This gets the Symbols column from the df Dataframe
-stockTicker=df["Symbol"]
+#Temp file that holds the updated values on if the stocks have options 
+tempFile = open("tempFile.csv", "w")
 
-#This splices the Dataframe to be ones we haven't checked yet
-stockTicker=stockTicker.iloc[int(pos):]
+#File to hold the tickers that recently have options added to them
+hasOptions = open("newOptions.txt", "w")
 
-i=1
-k=int(pos)
-#This is removing stocks with a price greater than 120
-#Also, this is assuming every stock over $120 has options and will be weekly
-try:
-    for ticker in stockTicker:
-        ticker = str(ticker)
-        ticker = ticker.strip()
-        price = get_live_price(ticker)
-        price = '%.2f' % price
-        print(k)
-        if float(price) > 120:
-            print(ticker + " Delete------------------------------------")
-            print(" Delete------------------------------------")
-            print(" Delete------------------------------------")
-            print(" Delete------------------------------------")
-    
+#This is how I initiated the prevLog file
+#for aline in finalFile:
+    #values = aline.split(",")
+    #val = values[0] + ",N\n"
+    #prevLog.write(val)
 
-        k = k + 1
-        if(i == 20):
-            file2 = open("myFile.txt", "w")
-            file2.truncate()
-            file2.write(str(k))
-            file2.close()
-            file2 = open("myFile.txt", "r")
-            i = 0
+#Formatted string for new run that goes into newOptions.txt file
+today = date.today()
+hasOptions.write("<--------------------> " + str(today) + " <-------------------->\n")
+
+
+i = 0
+for aline in finalFile:
+    values = aline.split(",")
+    try:
+        get_calls(values[0])
+        line = values[0] + ": added options recently.\n"
+        print(values[0] + " Yes")
+        cont = content[i]
+        conts = cont.split(",")
+        if conts[1] == "N\n":
+            print(line)
+            hasOptions.write(line)
+            hasOptions.flush()
             
-        i = i + 1
+            
+        tempFile.write(cont)
+        tempFile.flush()
+    except:
+        print(values[0] + " No")
+        tempFile.write(aline)
+        tempFile.flush()
+        continue
+    
+    i = i + 1
 
-except Exception as e: #Catch every exception so I can put the number I'm at currently into a text file
-    file2 = open("myFile.txt", "w")
-    file2.truncate()
-    file2.write(str(k))
-    file2.close()
-    print(e)
-    exit()
 
-#Got to end of file, now to set next time back to 0
-file2 = open("myFile.txt", "w")
-file2.truncate()
-file2.write("0")
-file2.close()
-#Checking if the stock has options
-has_options={}
-no_options={}
-i=0
-# for ticker in stockTicker:
-    #try:
-     #   has_options[ticker] = get_calls(ticker)
-    #except Exception:
-        #Need to add stuff here to update the file so if used tomorrow it knows yesterday it didn't
-        #print(ticker + " failed")
-        #no_options[i] = ticker
-     #   i = 1
+tempFile.flush()
+hasOptions.flush()
 
-#print(stockTicker)
-#print(no_options)
-print("---------------------------------------")
-#print(has_options)
+shutil.move(tempFile, previousLog)
+
+tempFile.close()
+hasOptions.close()
+prevLog.close()
+finalFile.close()
+
 
