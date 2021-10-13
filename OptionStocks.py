7eadbfefe33bcaa8ed25e9ced81708fede2d31d4
Dillon Gorlesky <49655577+dmgorlesky1@users.noreply.github.com>
2021-10-13 10:16:48 -0400

    Add files via upload
    Program is completed.

--- OptionStocks.py
+++ OptionStocks.py
@@ -1,15 +1,10 @@
 from yahoo_fin.options import *
-from yahoo_fin import options
 from yahoo_fin.stock_info import *
 import requests
 from pandas import *
-import os
-import sys
-import csv
-import math
-from difflib import Differ
 from datetime import date
 import shutil
+
 
 #STEPS
 #1. Read in file used to keep track of previous logs
@@ -25,63 +20,97 @@ import shutil
 #The file holding the previous logs of if a ticker had options or not last time the program was ran.
 prevLog = open("previousLog.csv", "r+")
 
-#This will allow for me to compare as the tickers are being checked for options
-content = prevLog.readlines()
-
 #The file all the filtered stocks get written to from NYSE and NASDAQ listings
 finalFile = open("finalTickers.csv", "r")
 
 #Temp file that holds the updated values on if the stocks have options 
 tempFile = open("tempFile.csv", "w")
+tempFile.truncate() #Making sure the temp files are empty before more info is added
 
 #File to hold the tickers that recently have options added to them
-hasOptions = open("newOptions.txt", "w")
+hasOptions = open("newOptions.txt", "r+")
+
+#Logs temporary output in order to keep track of previous logs
+tempOption = open("tempLogs.txt", "a+")
+tempOption.truncate() #Making sure the temp files are empty before more info is added
+
 
 #This is how I initiated the prevLog file
-#for aline in finalFile:
-    #values = aline.split(",")
-    #val = values[0] + ",N\n"
-    #prevLog.write(val)
+# for aline in finalFile:
+#     values = aline.split(",")
+#     val = values[0] + ",N\n"
+#     prevLog.write(val)
+
 
 #Formatted string for new run that goes into newOptions.txt file
 today = date.today()
-hasOptions.write("<--------------------> " + str(today) + " <-------------------->\n")
+tempOption.write("<--------------------> Tickers with New Options <-------------------->\n")
+tempOption.write("                             " + str(today) + "                       \n")
 
-
+k = 1
 i = 0
-for aline in finalFile:
-    values = aline.split(",")
-    try:
-        get_calls(values[0])
+
+for aline in prevLog:
+     values = aline.split(",")
+     print("Ticker " + str(k) + ": " + values[0]) #This is to print what ticker and # program is on.
+     k = k + 1
+     try:
+         get_calls(values[0])
+         print("After get calls")
         line = values[0] + ": added options recently.\n"
-        print(values[0] + " Yes")
-        cont = content[i]
-        conts = cont.split(",")
-        if conts[1] == "N\n":
-            print(line)
-            hasOptions.write(line)
-            hasOptions.flush()
-            
-            
-        tempFile.write(cont)
-        tempFile.flush()
+        if values[1] == "N\n":
+            tempOption.write(line)
+            tempOption.flush()
+            temp = values[0] + ",Y\n" #Updating file so next time it sees it does have options
+            tempFile.write(temp)
+            tempFile.flush()
+        else:      
+            tempFile.write(aline)
+            tempFile.flush()
     except:
-        print(values[0] + " No")
+        print("except")
         tempFile.write(aline)
         tempFile.flush()
         continue
-    
-    i = i + 1
-
-
+      
+ 
 tempFile.flush()
-hasOptions.flush()
+tempOption.flush()
 
-shutil.move(tempFile, previousLog)
 
-tempFile.close()
+#Adding spacing on temp file
+tempOption.write("\n\n")
+
+
+#Appending old logs onto temp file
+tempOption.write(hasOptions.read())
+tempOption.close()
+hasOptions.close()
+
+
+#Reopening in order to read and write, then move temp file to newOptions
+hasOptions = open("newOptions.txt", "r+")
+tempOption = open("tempLogs.txt", "r+")
+shutil.copyfile("tempLogs.txt", "newOptions.txt")
+tempOption.truncate() #Clearing temp file
+
+
+#Closing files
+tempOption.close()
 hasOptions.close()
 prevLog.close()
-finalFile.close()
+tempFile.close()
 
+
+#Moving new list of stocks with options back to previousLog file
+prevLog = open("previousLog.csv", "r+")
+tempFile = open("tempFile.csv", "r+")
+shutil.copyfile("tempFile.csv", "previousLog.csv")
+tempFile.truncate() #Clearing temp file
+
+
+#Final closing statements
+tempFile.close()
+prevLog.close()
+finalFile.close()
 
